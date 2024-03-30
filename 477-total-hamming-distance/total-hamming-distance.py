cc9c51ef0217786648e6c5035ea17dfaaf6322b2
SriSarvesh <54198507+SriSarveshA1@users.noreply.github.com>
2024-03-30 16:47:09 +0530

    Time: 353 ms (24.45%) | Memory: 12.7 MB (93.33%) - LeetSync

---
+++ 477-total-hamming-distance/total-hamming-distance.py
@@ -0,0 +1,34 @@
+class Solution(object):
+
+    def totalHammingDistance(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        n = len(nums)
+
+        count = 0
+
+        count_of_set_bits = [0 for i in range(0,32)] # count of set bits at each bit
+        count_of_unset_bits = [0 for i in range(0,32)] # n-count_of_set_bits[i] will be the count of unset bits in
+                                    # the provided list of numbers
+
+        for num in nums:
+            for i in range(0,31):
+                # there are total 32 bits.
+                # if we right shigt n>>i will give value of the ith bit
+                count_of_set_bits[i] += (num>>i)&1 # initially we will only count the no of set bits
+                # later we calculate the unset bits from the count_of_set_bits
+
+        
+
+        res = 0 # we can also calculate the overall answer 
+        for i in range(0,31):
+            count_of_unset_bits[i] = abs(n - count_of_set_bits[i])
+            res += count_of_set_bits[i] * count_of_unset_bits[i]
+
+        print(count_of_set_bits)
+        print(count_of_unset_bits)
+           
+        return res
