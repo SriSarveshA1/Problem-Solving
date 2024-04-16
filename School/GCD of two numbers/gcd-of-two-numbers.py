f4e06c942b8ea85a13a65d35ac7fb2a04ec9e88a
SriSarvesh <54198507+SriSarveshA1@users.noreply.github.com>
2024-04-16 20:22:40 +0530

    Updated Solution - GfG to GitHub

--- School/GCD of two numbers/gcd-of-two-numbers.py
+++ School/GCD of two numbers/gcd-of-two-numbers.py
@@ -2,25 +2,13 @@
 class Solution:
     def gcd(self, a : int, b : int) -> int:
         # code here
-        if a==0:
-            return b
-            
-        if b==0:
-            return a
-            
         divisor = min(a,b)
         divident = max(a,b)
-        
-
-        
-        while True:
-            
-            if divident % divisor == 0:
+        while(True):
+            if ( (divident % divisor) == 0 ):
                 return divisor
-            
-            if divident % divisor == 1:
+            if ( (divident % divisor) == 1 ):
                 return 1
-            
             
             reminder = divident % divisor
             divident = divisor
