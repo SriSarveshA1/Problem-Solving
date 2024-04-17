22ab284eef217da6668e0ad5779950f460dadb01
SriSarvesh <54198507+SriSarveshA1@users.noreply.github.com>
2024-04-17 17:58:43 +0530

    Time: 42 ms (18.38%) | Memory: 16.6 MB (57.14%) - LeetSync

--- 342-power-of-four/power-of-four.py
+++ 342-power-of-four/power-of-four.py
@@ -4,11 +4,8 @@ class Solution:
             return True
         if (n<4):
             return False
-        
-        no_of_bits_that_make_up = int(math.log2(n)) + 1 
-        print("no_of_bits_that_make_up = ",no_of_bits_that_make_up)
-
-        if (n&(n-1)) == 0 and (no_of_bits_that_make_up%2 != 0):
+       
+        if (n&(n-1)) == 0 and (n%3 == 1):
             return True
         else:
             return False
