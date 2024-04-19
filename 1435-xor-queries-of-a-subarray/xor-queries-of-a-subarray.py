f7998c532b500076b78741e38fc1c1e0e05a4b36
SriSarvesh <54198507+SriSarveshA1@users.noreply.github.com>
2024-04-19 17:41:48 +0530

    Time: 288 ms (96.97%) | Memory: 31.2 MB (91.67%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.py
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.py
@@ -1,39 +1,17 @@
-class Solution(object):
+class Solution:
+    def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
+        xor = 0 
+        prefix_xor = [ 0 for i in range(0,len(arr))]
+        prefix_xor[0] = arr[0]
+        for i in range(1,len(arr)):
+            prefix_xor[i] = prefix_xor[i-1] ^ arr[i]
 
-    def find_xor_in_range(self,arr,left,right):
-        xor = 0
-        for i in range(left,right+1):
-            xor = (xor ^ arr[i])%1
-
-        return xor
-
-    def xorQueries(self, arr, queries):
-        """
-        :type arr: List[int]
-        :type queries: List[List[int]]
-        :rtype: List[int]
-        """
-        res = []
-
-        n = len(arr)
-
-        prefix_xor = []
-        prefix_xor.append(arr[0])
-
-        for i in range(1,n):
-            prefix_xor.append(prefix_xor[i-1] ^ arr[i])
-
-
+        result = []
         for query in queries:
-            left = query[0]
-            right = query[1]
-
-            if left == 0:
-                temp = prefix_xor[right]
+            l = query[0]
+            r = query[1]
+            if l != 0:
+                result.append(prefix_xor[r]^prefix_xor[l-1])
             else:
-                temp = prefix_xor[right] ^ prefix_xor[left-1]
-
-            res.append(temp)
-
-        return res
-        
+                result.append(prefix_xor[r])
+        return result
