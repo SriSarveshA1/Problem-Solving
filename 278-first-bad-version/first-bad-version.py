fce951426f362846b44ceb54cfa98ebf55584ef9
SriSarvesh <54198507+SriSarveshA1@users.noreply.github.com>
2024-05-02 20:16:18 +0530

    Time: 32 ms (75.01%) | Memory: 16.7 MB (12.45%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.py
@@ -0,0 +1,24 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+
+    def find_mid(self,low,high):
+        if((low+high+1)%2 == 0):
+            return int(low+((high-low)/2))
+
+        return int(low+((high-low+1)/2))
+
+    def firstBadVersion(self, n: int) -> int:
+        low = 1
+        high = n
+
+        while (low<=high):
+            mid = self.find_mid(low,high)
+            print("l = ",low," r = ",high," mid = ",mid)
+            if isBadVersion(mid):
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return low
