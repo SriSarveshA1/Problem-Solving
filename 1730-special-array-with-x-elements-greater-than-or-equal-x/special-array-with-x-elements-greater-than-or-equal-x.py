1125445f88aa76fa35ae5890b0a80b9a1961731c
SriSarvesh <54198507+SriSarveshA1@users.noreply.github.com>
2024-05-27 23:14:58 +0530

    Time: 26 ms (39.68%) | Memory: 11.5 MB (79.37%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.py
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.py
@@ -15,9 +15,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-     
+        max_num = max(nums)
         
-        for i in range(1,1001):
+        for i in range(1,max_num+1):
             num = i
             if self.find_no_occ(nums,num) == num:
                     return num
