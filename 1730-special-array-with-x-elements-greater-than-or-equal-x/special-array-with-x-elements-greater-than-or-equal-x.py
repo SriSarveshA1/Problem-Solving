1b5be410033b9f0c5f41b94386bfeac0b28d9049
SriSarvesh <54198507+SriSarveshA1@users.noreply.github.com>
2024-05-27 23:14:53 +0530

    Time: 28 ms (22.22%) | Memory: 11.7 MB (30.16%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.py
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.py
@@ -15,20 +15,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        hash_map = dict()
-        for num in nums:
-            if num not in hash_map:
-                hash_map[num] = 1
-            else:
-                hash_map[num] += 1
-
-        for key in hash_map:
-            num = key
-            count = hash_map[num]
-
-            if count == 1:
-                if self.find_no_occ(nums,num) == num:
-                    return num
+     
         
         for i in range(1,1001):
             num = i
