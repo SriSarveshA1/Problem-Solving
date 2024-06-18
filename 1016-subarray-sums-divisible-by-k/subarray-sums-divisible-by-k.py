b0c9ab9e7437d609364783fa5dae01aa217ad0c7
SriSarvesh <54198507+SriSarveshA1@users.noreply.github.com>
2024-06-18 06:55:36 +0530

    Time: 249 ms (22.72%) | Memory: 21.4 MB (74.07%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
@@ -2,19 +2,26 @@ class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
         prefix_array = [0 for i in range(len(nums))]
         prefix_array[0]=nums[0]
+        
         for i in range(1,len(nums)):
             prefix_array[i] = prefix_array[i-1] + nums[i]
-        
+        print(prefix_array)
         for i in range(0,len(nums)):
             prefix_array[i] = prefix_array[i] % k
-        print("pre ",prefix_array)
+       
         hash_map = dict()
         count = 0
+        
         for i in range(0,len(nums)):
             num_mod_k = prefix_array[i] % k
-            if num_mod_k == 0:
-                count+=1
+            
+            if num_mod_k == 0: # corner case
+                count+=1 # To find the subarray from [0,i]
+                        # The subarray from ([0,i] % k) ==0
+                        # We are counting this seperately
+
             if num_mod_k in hash_map:
+                # finding whether subarray[i] that exists or not
                 count+= hash_map[num_mod_k]
                 hash_map[num_mod_k] = hash_map[num_mod_k] + 1
             else:
