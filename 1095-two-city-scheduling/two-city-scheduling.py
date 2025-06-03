7becaef55ee07a235c7c217739b44097bce3356b
SriSarvesh <54198507+SriSarveshA1@users.noreply.github.com>
2025-06-03 08:27:51 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (93.12%) - LeetSync

--- 1095-two-city-scheduling/two-city-scheduling.py
+++ 1095-two-city-scheduling/two-city-scheduling.py
@@ -2,13 +2,6 @@ from functools import cmp_to_key
 
 class Solution:
 
-    def compare_func(self,value_1,value_2):
-        if abs(value_1[0]-value_1[1]) > abs(value_2[0]-value_2[1]):
-            return 1
-        elif abs(value_1[0]-value_1[1]) < abs(value_2[0]-value_2[1]):
-            return -1
-        else:
-            return 0
 
 
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
