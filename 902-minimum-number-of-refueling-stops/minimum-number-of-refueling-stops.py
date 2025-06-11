6452be5ad16ea8a62abe279059b5358845de430f
SriSarvesh <54198507+SriSarveshA1@users.noreply.github.com>
2025-06-11 21:27:51 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (5.44%) - LeetSync

--- 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.py
+++ 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.py
@@ -1,21 +1,54 @@
+from heapq import heappop, heappush, heapify
+
 class Solution:
+
+    def move_cur_l_pointer_return(self, l, stations, max_heap, cur_pos):
+        n = len(stations)
+        while (l+1<n and cur_pos>=stations[l+1][0]):
+            heappush(max_heap,-1 * stations[l+1][1])
+            l = l+1
+        
+        return l
+
+
     def minRefuelStops(self, target: int, start_fuel: int, stations: List[List[int]]) -> int:
-        if start_fuel >= target:
-            return 0
-
         max_heap = []
-        i, n = 0, len(stations)
-        stops = 0
-        max_distance = start_fuel
+        heapify(max_heap)
+        
+        if target - start_fuel == 0:
+            return 0
+        
+        cur_pos = 0
 
-        while max_distance < target:
-            if i < n and stations[i][0] <= max_distance:
-                heapq.heappush(max_heap, -stations[i][1])
-                i += 1
-            elif not max_heap:
-                return -1
-            else:
-                max_distance += -heapq.heappop(max_heap)
-                stops += 1
+        no_of_stops = 0
 
-        return stops
+        cur_fuel = start_fuel
+
+        l = -1
+
+        while cur_pos < target:
+            cur_pos += cur_fuel 
+
+            if cur_pos >= target:
+                print(max_heap)
+
+                return no_of_stops
+            elif cur_pos < target:
+                cur_fuel = 0
+
+
+                l = self.move_cur_l_pointer_return(l,stations,max_heap,cur_pos)
+                
+                if len(max_heap) == 0:
+                    return -1
+
+                cur_fuel += (-1 * heappop(max_heap))
+                no_of_stops+=1
+        print(max_heap)
+        return no_of_stops
+
+
+
+
+
+
