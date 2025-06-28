7de1f4155a578ddeea7a18321c79bdd0f928f14d
SriSarvesh <54198507+SriSarveshA1@users.noreply.github.com>
2025-06-28 16:57:02 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (9.33%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -5,6 +5,14 @@ class Solution:
 
         return int(low+(high-low+1)/2)
 
+    def is_valid_mini(self,arr,mid):
+        n = len(arr)
+        if mid-1 < 0 and mid+1 <=n:
+            return True
+        
+        if mid-1>=0 and arr[mid-1]>arr[mid]:
+            return True
+
     def findMin(self, arr: List[int]) -> int:
         n = len(arr)
         l = 0
@@ -13,14 +21,30 @@ class Solution:
         while(l<=r):
             mid = self.find_mid(l,r)
 
-            if arr[l]<=arr[mid] and arr[mid]<=arr[r]:
-                return arr[l]
+            if l==r:
+                return arr[mid]
             
-            if arr[l]<=arr[mid] and arr[l]>arr[r]:
-                l = mid+1
-            else:
+            if mid-1 < 0 and arr[mid]<arr[mid+1]:
+                return arr[mid]
+            
+            if mid-1 >= 0:
                 if arr[mid-1]>arr[mid]:
                     return arr[mid]
-                r = mid-1
+                
+            if arr[l] <= arr[mid]:
+                if arr[l] <= arr[r]:
+                    r = mid-1
+                else:
+                    l = mid+1
+            else:
+                if arr[mid] <= arr[r]:
+                    r = mid-1
+                else:
+                    l = mid+1
+        
+            
+            
+            
+
         
         
