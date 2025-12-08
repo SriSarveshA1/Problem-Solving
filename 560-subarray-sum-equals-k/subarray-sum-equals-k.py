f9383f8462c2ca4ac2ff45e8221cde7aaad1c83a
SriSarvesh <54198507+SriSarveshA1@users.noreply.github.com>
2025-12-08 19:11:51 +0530

    Time: 50 ms (16.46%) | Memory: 16 MB (10.57%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,35 +1,39 @@
 class Solution(object):
-    def subarraySum(self, arr, k):
+    def subarraySum(self, nums, target):
         """
         :type nums: List[int]
         :type k: int
         :rtype: int
         """
-        hash_map = dict()
-        
-        prefix_sum = arr[0]
-        
-
+        n = len(nums)
         count = 0
-
-        hash_map[prefix_sum] = 1
         
-        if prefix_sum == k:
-            count+=1
+        prefix_sum = [0 for i in range(n)]
         
-        for i in range(1,len(arr)):
+        freq_map = dict()
+        
+        prefix_sum[0] = nums[0]
+        freq_map[prefix_sum[0]] = 1
+        
+        if prefix_sum[0] == target:
+            count += 1
+        
+        # need to find the number of subarrays matching the target 
+        
+        for i in range(1,n):
+            prefix_sum[i] = prefix_sum[i-1]+nums[i]
             
-            prefix_sum += arr[i]
-            if prefix_sum == k:
+            if prefix_sum[i] == target:
                 count+=1
             
-            if(prefix_sum-k) in hash_map:
-                count+=hash_map[prefix_sum-k]
-
-
-            if prefix_sum not in hash_map:
-                hash_map[prefix_sum] = 1
+            if prefix_sum[i]-target in freq_map:
+                count+= freq_map[prefix_sum[i]-target]
+                
+            
+            if prefix_sum[i] in freq_map:
+                freq_map[prefix_sum[i]] += 1
             else:
-                hash_map[prefix_sum]=hash_map[prefix_sum]+1
+                freq_map[prefix_sum[i]] = 1 
         
         return count
+        
