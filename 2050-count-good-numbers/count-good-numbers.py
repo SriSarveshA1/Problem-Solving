459d4bccdba7d875ca266c5bcad26091138162a7
SriSarvesh <54198507+SriSarveshA1@users.noreply.github.com>
2025-12-25 20:54:06 +0530

    Time: 3 ms (20.45%) | Memory: 17.5 MB (91.85%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.py
+++ 2050-count-good-numbers/count-good-numbers.py
@@ -1,53 +1,51 @@
-class Solution(object):
+import math
 
-    def calculate_powers(self,x,n):
+class Solution:
+    def temp(self,x,N):
         mod = 1000000007
 
-        if n == 0:
-            return 1
-
-        if n == 1:
+        if N==1:
             return x
         
-        ans = self.calculate_powers(x,n//2)
-
-        if n%2 == 0:
-            return ((ans%mod) * (ans%mod)) % mod
+        if N==0:
+            return 1
         
-        return ((ans%mod)*(ans%mod)*(x%mod)) % mod
 
-    def countGoodNumbers(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        mod = 1000000007
+        ans = self.temp(x, math.floor(N/2))
 
+        if (N%2) == 0:    
+            return (ans*ans) % mod
+        else:
+            return (ans*ans*x) % mod
+
+    def countGoodNumbers(self, n: int) -> int:
         if n == 1:
-            return 5
+            return 5 
+        
+        if n == 2:
+            return 20
+        
+        mod = 1000000007
 
         no_of_even = None
         no_of_odd = None
         
-        if n%2 != 0:
-            no_of_even = math.floor(((n-1)/2)+1)
+        if n%2 == 0:
+            # even
+            print("in even")
+            no_of_even = math.ceil((n-1)/2)
+            print("///event",no_of_even)
             no_of_odd = n - no_of_even
+            print("///no of odd",no_of_odd)
         else:
-            no_of_even = n/2
-            no_of_odd = no_of_even
+            print("in odd")
+            no_of_even = math.ceil(n/2)
+            no_of_odd = n - no_of_even
         
         print("no_of_even = ",no_of_even)
         print("no_of_odd = ",no_of_odd)
 
-        pow_5 = self.calculate_powers(5,no_of_even) 
-        pow_4 = self.calculate_powers(4,no_of_odd)
-
-        print("pow_5 = ",pow_5)
-        print("pow_4 = ",pow_4)
-
-        return (pow_4  * pow_5 ) % mod
-
-        
-        
-
-        
+        if (n%2)!=0:
+            return (self.temp(5,no_of_even) * self.temp(4,no_of_odd)) % mod
+        else:
+            return (self.temp(5,no_of_even) * self.temp(4,no_of_even)) % mod
