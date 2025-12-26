9f486f6588a63d90518b517225e3f71b9fa3d199
SriSarvesh <54198507+SriSarveshA1@users.noreply.github.com>
2025-12-26 18:09:47 +0530

    Time: 2 ms (26.74%) | Memory: 17.2 MB (96.88%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,41 +1,25 @@
-class Solution(object):
-
-    def find_uniq_paths(self,i,j,m,n):
-        if (i >= m ) or (j>=n):
-            return 0
-
-        if (i == m-1) and (j == n-1):
-            return 1
-
-        ans1 = self.find_uniq_paths(i+1,j,m,n)
-        ans2 = self.find_uniq_paths(i,j+1,m,n)
-        return ans1+ans2
-
-    def uniquePaths(self, m, n):
-        """
-        :type m: int
-        :type n: int
-        :rtype: int
-        """
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
         
-        dp = [[-1 for i in range(n)] for j in range(m)]
+        dp = [ [0 for j in range(n)] for i in range(m) ]
 
         dp[0][0] = 1
+
         for i in range(0,m):
             for j in range(0,n):
-                if i == 0 and j==0:
+                if i == 0 and j == 0:
                     continue
-
-                sum1 = 0
-                sum2 = 0
-
-                if j-1>=(0):
-                    sum1 = dp[i][j-1]
                 
-                if i-1>=(0):
-                    sum2 = dp[i-1][j]
+                ans1 = 0 # [i-1][j]
+                ans2 = 0 # [i][j-1]
 
-                dp[i][j] = sum1 + sum2
+                if i-1 >= 0:
+                    ans1 = dp[i-1][j]
+                
+                if j-1 >=0:
+                    ans2 = dp[i][j-1]
+                
+                dp[i][j] = ans1+ans2
+
         
-        print(dp)
         return dp[m-1][n-1]
