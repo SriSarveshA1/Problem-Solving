c55ca51b02fa147d459ffe48c4d2c7863853d131
SriSarvesh <54198507+SriSarveshA1@users.noreply.github.com>
2025-12-26 06:40:07 +0530

    Time: 3 ms (57.23%) | Memory: 17.5 MB (91.76%) - LeetSync

--- 93-restore-ip-addresses/restore-ip-addresses.py
+++ 93-restore-ip-addresses/restore-ip-addresses.py
@@ -1,5 +1,4 @@
 class Solution:
-
     def validate_ip_part(self,string):
         # 1.CHeck for trailing zeros
         # 2. ANd check for the ip part range
@@ -14,15 +13,12 @@ class Solution:
             return True
         
         return False
-    
     def validate_whole_ip_address(self,cur)->(bool,str):
         if len(cur)!=4:
-            # No of parts is greater than 4
+            # No of parts is not equal to 4
             return False,""
         
         return True, ".".join(cur)
-
-
     def generateIpAddresses(self,cur,res,start,s,n):
         if start>=n:
             # If we reach the terminal
@@ -32,20 +28,20 @@ class Solution:
                 res.append(string)
             
             return
+        
+        if len(cur)>4: #if the length of the parts has gone beyond 4 ,evn though still we havn't finished exploring the end of the string
+            return 
 
         for i in range(start,n):
 
-            if ((i+1) - start)+1 >4:
-                break
+            if ((i+1) - start)+1 >4:# We do this because if we are in path where we are considering a substring which beyond the valid ip length
+                break               # we break that path, as it won't give valid results
 
             if self.validate_ip_part(s[start:i+1]):
                 cur.append(s[start:i+1])
                 self.generateIpAddresses(cur,res,i+1,s,n)
                 cur.pop()
-
-
     def restoreIpAddresses(self, s: str) -> List[str]:
-        
         cur = []
         res = []
         n = len(s)
