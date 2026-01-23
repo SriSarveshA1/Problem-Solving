c6c85c906900fe3a0d7dd7cc2488a8e4fbc09a57
SriSarvesh <54198507+SriSarveshA1@users.noreply.github.com>
2026-01-23 06:33:37 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (8.21%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,50 +1,48 @@
+import math
+
 class Solution:
-    def find_mid(self,low ,high):
-        if (low+high+1)%2 == 0:
-            return int(low+(high-low)/2)
 
-        return int(low+(high-low+1)/2)
+    def calculate_mid(self,l,r):
+        mid = math.floor(l + ((r-l)/2))
+        return mid
 
-    def is_valid_mini(self,arr,mid):
-        n = len(arr)
-        if mid-1 < 0 and mid+1 <=n:
+    
+    def is_min(self,nums,n,mid):
+        if (mid-1 < 0) and (mid+1 > n-1):
             return True
         
-        if mid-1>=0 and arr[mid-1]>arr[mid]:
+        if (mid-1<0) and (mid+1 <=n-1) and (nums[mid]<nums[mid+1]):
             return True
+        
+        if (mid-1>=0) and (mid+1 > n-1) and (nums[mid-1] > nums[mid]):
+            return True
+        
+        if (mid-1>=0 and mid+1<=n-1 and nums[mid-1]>nums[mid] and nums[mid]<nums[mid+1]):
+            return True
+        
 
-    def findMin(self, arr: List[int]) -> int:
-        n = len(arr)
+
+    def findMin(self, nums: List[int]) -> int:
+        n = len(nums)
         l = 0
         r = n-1
 
         while(l<=r):
-            mid = self.find_mid(l,r)
+            mid = self.calculate_mid(l,r)
 
-            if l==r:
-                return arr[mid]
+            if self.is_min(nums,n,mid):
+                return nums[mid]
             
-            if mid-1 < 0 and arr[mid]<arr[mid+1]:
-                return arr[mid]
-            
-            if mid-1 >= 0:
-                if arr[mid-1]>arr[mid]:
-                    return arr[mid]
-                
-            if arr[l] <= arr[mid]:
-                if arr[l] <= arr[r]:
-                    r = mid-1
-                else:
+            if nums[l] <= nums[mid]:
+                if nums[l]>nums[r]:
                     l = mid+1
+                else:
+                    r = mid-1
             else:
-                if arr[mid] <= arr[r]:
-                    r = mid-1
-                else:
-                    l = mid+1
-        
-            
-            
-            
+                r = mid-1
 
-        
+        return -1 
+
+
+
         
