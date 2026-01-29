866e23cc532bf9a28ab2bb4fe8a0532885ab44ca
SriSarvesh <54198507+SriSarveshA1@users.noreply.github.com>
2026-01-29 07:27:50 +0530

    Time: 95 ms (43.56%) | Memory: 31.1 MB (17.82%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -3,19 +3,29 @@ import heapq
 
 class Solution:
     def push(self,pq,ele):
-        heapq.heappush(pq,-ele)
+        # Trying to do min heap by adding the negative values
+        heapq.heappush(pq,ele)
 
     def pop(self,pq):
-        return -heapq.heappop(pq)
+        return heapq.heappop(pq)
+
+    def peek(self,pq):
+        return pq[0]
 
     def findKthLargest(self, nums: List[int], k: int) -> int:
         pq = []
+
         for num in nums:
+            #print(pq)
+            if len(pq)==k and self.peek(pq) > num:
+                continue
+
             self.push(pq,num)
+
+            if len(pq)>k:
+                self.pop(pq)
         
-        ans = None
-        for i in range(1,k+1):
-            ans = self.pop(pq)
+        return self.pop(pq)
         
-        return ans
+       
         
