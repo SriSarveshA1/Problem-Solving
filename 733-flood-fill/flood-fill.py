f6fffa8d1a4bf397bc4864abbf2625fddef17ed6
SriSarvesh <54198507+SriSarveshA1@users.noreply.github.com>
2026-02-03 07:09:26 +0530

    Time: 1 ms (23.78%) | Memory: 19.5 MB (29.47%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,44 +1,57 @@
-class Solution(object):
-    
-    def modify_image(self,m,n,matrix,start_color,target_color,i,j):
-        if i>=m or i<0 or j>=n or j<0:
-            return
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        # initial color = image[sr][sc]
         
-        cur_color = matrix[i][j]
+        # We do bfs from (sr,sc) and add the nodes to the queue whose value is equal to initial color(we can use this
+        # as the visiting array alternative)
+
+        # While visiting the node we mark it as color value
+
+        initial_color = image[sr][sc]
+
+        queue = []
+
+        image[sr][sc] = color # marked the initial node as visited
+        queue.append((sr,sc))
 
 
-        if cur_color != start_color or cur_color == target_color:
-            return
+        m = len(image)
+        n = len(image[0])
+
+        visited = [[False for j in range(n)] for i in range(m)]
+
+        while(len(queue)!=0):
+            i,j = queue.pop(0)
+
+            # Now iterate the adj nodes of this (i,j)
+
+            # (i+1,j)
+            if i+1<=m-1:
+                if not visited[i+1][j] and  image[i+1][j] == initial_color: #We can consider this as unvisited logic and add it to bfs
+                    queue.append((i+1,j))
+                    image[i+1][j] = color # Its like marking it as visited
+                    visited[i+1][j] = True
+
+            # (i-1,j)
+            if i-1>=0:
+                if not visited[i-1][j] and image[i-1][j] == initial_color:
+                    queue.append((i-1,j))
+                    image[i-1][j] = color
+                    visited[i-1][j] = True
+
+            # (i,j+1)
+            if j+1<=n-1:
+                if not visited[i][j+1] and image[i][j+1] == initial_color:
+                    queue.append((i,j+1))
+                    image[i][j+1] = color
+                    visited[i][j+1]=True
+
+            # (i,j-1)
+            if j-1>=0:
+                if not visited[i][j-1] and image[i][j-1] == initial_color:
+                    queue.append((i,j-1))
+                    image[i][j-1] = color
+                    visited[i][j-1]=True
         
-        if cur_color == start_color:
-            matrix[i][j] = target_color
-        
-        self.modify_image(m,n,matrix,start_color,target_color,i,j-1)
-        self.modify_image(m,n,matrix,start_color,target_color,i,j+1)
-        self.modify_image(m,n,matrix,start_color,target_color,i-1,j)
-        self.modify_image(m,n,matrix,start_color,target_color,i+1,j)
-
-
-
-    def floodFill(self, matrix, sr, sc, color):
-        """
-        :type image: List[List[int]]
-        :type sr: int
-        :type sc: int
-        :type color: int
-        :rtype: List[List[int]]
-        """
-        start_color = matrix[sr][sc]
-        target_color = color
-
-        m = len(matrix)
-        n = len(matrix[0])
-
-        self.modify_image(m,n,matrix,start_color,target_color,sr,sc)
-        return matrix
-        
-
-        
-
-
-        
+        return image
+            
