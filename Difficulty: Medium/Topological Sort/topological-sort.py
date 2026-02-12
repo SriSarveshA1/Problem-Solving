f79bba15b2bc7abf2079702f458ff9174dbff37b
SriSarvesh <54198507+SriSarveshA1@users.noreply.github.com>
2026-02-12 10:34:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.py
+++ Difficulty: Medium/Topological Sort/topological-sort.py
@@ -1,44 +1,56 @@
 class Solution:
-    
-    def dfs(self,node,adj_list,visited,stack):
-        for adj_node in adj_list[node]:
-            # We visit all the adj node of this particular node
-            if not visited[adj_node]:
-                visited[adj_node] = True
-                self.dfs(adj_node,adj_list,visited,stack)
+    def bfs(self,adj_list,indegree,queue,res):
         
-        #print("//--",node)
-        # Once all are explored 
-        stack.append(node) # we push that element to the stack
+        while(len(queue)!=0):
+            node = queue.pop(0)
+            res.append(node)
+            
+            for adj_node in adj_list[node]:
+                indegree[adj_node]-=1
+                
+                if indegree[adj_node] == 0:
+                    queue.append(adj_node)
+            
         
+        
+        
+                
+                
     
     def topoSort(self, V, edges):
         # Code here
         
         adj_list = [[] for i in range(V)]    
-        visited = [False for i in range(V)]
-        stack = []
+        indegree = [0 for i in range(V)]
+        res = []
+        queue = []
         
         # construct the adj_list
+        # Along with that we also construct the indegree
         for edge in edges:
             x,y = edge
             adj_list[x].append(y)
+            indegree[y]+=1
+            
+        
+        #1.Then we iterate all the nodes in the indegree and put the nodes which has 0 indegree 
+        #into the queue
+        for node in range(0,V):
+            if indegree[node] == 0:
+                queue.append(node)
+        
+        
+        #2.Then seperately we pop from queue(and add it to result set) ,and reduce the indegree of all the adj nodes of that
+        # particular nodes, and if any adj_node's indegree becomes 0 we add it to the queue
+        
+        #3. We continue this until queue becomes empty
+        self.bfs(adj_list,indegree,queue,res)
+        return res
+        
+        
+        
+        
 
-        #print("adj_list",adj_list)
-        
-        for v in range(0,V):
-            if not visited[v]:
-                visited[v]=True
-                self.dfs(v,adj_list,visited,stack)
-        
-        res = []
-        #print("...stack",stack)
-        # Pop the elements from the stack
-        for i in range(V-1,-1,-1):
-            res.append(stack[i])
-        
-        #print(".....",res)
-        
         return res
         
         
