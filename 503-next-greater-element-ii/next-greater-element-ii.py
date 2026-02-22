83cbe905e2a80cf2d35e36032bf7e00890f5b73d
SriSarvesh <54198507+SriSarveshA1@users.noreply.github.com>
2026-02-22 11:37:47 +0530

    Time: 60 ms (8.72%) | Memory: 21.2 MB (49.94%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        
+        stack = []
+
+        res = []
+
+        hash_map = dict()
+
+        n = len(nums)
+
+        for i in range((2*n)-1,-1,-1):
+
+            while len(stack) != 0 and stack[len(stack)-1]<= nums[i%n]:
+                stack.pop(len(stack)-1) 
+            
+            if i%n not in hash_map or hash_map[i%n] == -1 :
+                if len(stack) == 0:
+                    hash_map[i%n] = -1
+                else:
+                    hash_map[i%n] = stack[len(stack)-1]
+
+            stack.append(nums[i%n])
+
+        for i in range(0,len(nums)):
+            res.append(hash_map[i])
+        
+        return res
+                
+
