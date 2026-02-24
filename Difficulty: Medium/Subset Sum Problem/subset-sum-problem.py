21a044921b43e999959195bc09ada0129ae13667
SriSarvesh <54198507+SriSarveshA1@users.noreply.github.com>
2026-02-24 15:59:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.py
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.py
@@ -1,29 +1,50 @@
 class Solution:
     
-    def find_subset_exist(self,n,arr,i,target):
-        
-        if target <0:
-            return False
-        
-        if i>=n:
-            if target == 0:
-                return True
-            
-            return False
-        
-        pick = self.find_subset_exist(n,arr,i+1,target-arr[i])
-        
-        not_pick = False
-        if not pick:
-            not_pick = self.find_subset_exist(n,arr,i+1,target)
-        
-        return not_pick or pick
-        
-        
+    
     def isSubsetSum (self, arr, target):
         # code here 
-        
         n = len(arr)
-        return self.find_subset_exist(n,arr,0,target)
+        dp = [[False for j in range(0,target+1)] for i in range(n)]
+
+        # i-> 0 to n represents the index in arr,
+        # j -> 0 to target represent all the values between 0 to target
+        # Basically the values between j -> 0 represents the target values
         
+        for i in range(0,n):
+            for j in range(0,1):
+                # Only the first column alone all the values will be True,
+                # Because its like using the elements from [0 to i] are we able to achieve the target or not
+                dp[i][j] = True
+                # This will be True for the first column only because, just by not picking any element
+                # within 0 to i we can reach the target 0.
+            
+  
+        for i in range(0,1):
+        # And as per the base case when i == 0, when we are at the first row
+        # its like if the particular i=0 th index value in the arr is matching with target then it will be True 
+        # else False
+            for j in range(0,target+1):
+                if arr[i] == j:
+                    dp[i][j] = True
+        
+        
+        # We start from the 2nd row and try to compute for each i,j
+        # dp[i][j] = dp[i-1][j] or dp[i-1][j-arr[i]]
+        
+        for i in range(1,n):
+            for j in range(1,target+1):
+                
+                not_pick = dp[i-1][j]
+                pick = False
+                if (j-arr[i])>=0:
+                    pick= dp[i-1][j-arr[i]]
+                
+                dp[i][j] = pick or not_pick
+        
+        #print("...",dp[n-1])
+        return dp[n-1][target]
+                
+                
+        
+
         
