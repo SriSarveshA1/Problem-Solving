bb36ff26a122a0ab86cb1c276ee80f9496a8121c
SriSarvesh <54198507+SriSarveshA1@users.noreply.github.com>
2026-03-29 22:28:19 +0530

    Time: 358 ms (45.80%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select q.query_name , round(avg(q.rating/q.position),2) as quality,coalesce(round(sum(case when rating <3 then 1 end)/count(*) * 100,2),0)  as poor_query_percentage  from queries q group by q.query_name;
